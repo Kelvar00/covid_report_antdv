d1c4682350739829c8a5810b951cee01be0d24db
kelvar00 <1404430112@qq.com>
2023-08-07 12:59:38 +0800

    202308071259

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 declare var require: any
-import { ref, onMounted, type Ref } from 'vue'
+import { ref, onMounted, type CSSProperties } from 'vue'
 import fullpage from 'fullpage.js'
-import { ArrowRight, CaretBottom, CaretTop, Warning } from '@element-plus/icons-vue'
 import Image1 from '@/assets/img/T22.png'
 import Image2 from '@/assets/img/T23.png'
 import Image3 from '@/assets/img/T24.png'
@@ -72,23 +71,53 @@ import MapCharts from './components/MapCharts.vue'
 import MapTimeline from './components/MapTimeline.vue'
 import TrendTimeline from './components/TrendTimeline.vue'
 //import { requiredNumber } from 'element-plus/es/components/table-v2/src/common.js';
+const transparentStyle: CSSProperties = {
+  color: 'transoarent',
+  backgroundColor: 'transparent'
+}
+const headerStyle: CSSProperties = {
+  textAlign: 'center',
+  color: 'transparent',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: 'transparent'
+}
+
+const contentStyle: CSSProperties = {
+  textAlign: 'center',
+  color: '#00000000',
+  backgroundColor: 'transparent'
+}
+
+const siderStyle: CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9'
+}
+
+const footerStyle: CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea'
+}
 </script>
 
 <template>
-  <el-carousel
+  <a-carousel
     indicator-position="none"
-    height="100%"
     style="width: 100%; height: 100%; position: absolute"
     :autoplay="true"
   >
-    <el-carousel-item v-for="item in carouseData" :key="item">
+    <div v-for="item in carouseData" :key="item">
       <img :src="item.url" alt="" style="object-fit: initial; height: 100%; width: 100%" />
-    </el-carousel-item>
-  </el-carousel>
+    </div>
+  </a-carousel>
   <div id="fullpage">
     <div class="section">
-      <el-container>
-        <el-header>
+      <a-layout :style="transparentStyle">
+        <a-layout-header :style="headerStyle">
           <div
             style="
               text-align: center;
@@ -100,92 +129,70 @@ import TrendTimeline from './components/TrendTimeline.vue'
           >
             R . I . P
           </div>
-        </el-header>
-        <el-main>
-          <div style="margin-bottom: 200px"><el-col :span="24"></el-col></div>
-        </el-main>
-        <el-main>
-          <el-scrollbar>
+        </a-layout-header>
+        <a-layout-content :style="contentStyle">
+          <div style="margin-bottom: 200px"><a-col :span="24"></a-col></div>
+        </a-layout-content>
+        <a-layout-content :style="contentStyle">
+          <div class="scroll_content">
             <div class="content-padding-1">
-              <el-col :span="24"> </el-col>
+              <a-col :span="24"> </a-col>
             </div>
-            <el-row :gutter="12">
-              <el-col :span="8">
+            <a-row style="max-width: 100vw" :gutter="12">
+              <a-col :span="8">
                 <div class="statistic-card">
-                  <el-statistic :value="168983095">
+                  <a-statistic :value="168983095">
                     <template #title>
-                      <div style="display: inline-flex; align-items: center">
-                        confirmed cases
-                        <el-tooltip
-                          effect="dark"
-                          content="Number of people infected in the pandemic"
-                          placement="top"
-                        >
-                          <el-icon style="margin-left: 4px" :size="12">
-                            <Warning />
-                          </el-icon>
-                        </el-tooltip>
-                      </div>
+                      <div style="display: inline-flex; align-items: center">confirmed cases</div>
                     </template>
-                  </el-statistic>
+                  </a-statistic>
                   <div class="statistic-footer">
                     <div class="footer-item">
                       <span>Lat update:2 August 2023 at 07:56 pm GMT+8</span>
                     </div>
                   </div>
                 </div>
-              </el-col>
-              <el-col :span="8">
+              </a-col>
+              <a-col :span="8">
                 <div class="statistic-card">
-                  <el-statistic :value="6953743">
+                  <a-statistic :value="6953743">
                     <template #title>
-                      <div style="display: inline-flex; align-items: center">
-                        Confirmed deaths
-                        <el-tooltip
-                          effect="dark"
-                          content="Number of people dead from ncov-19"
-                          placement="top"
-                        >
-                          <el-icon style="margin-left: 4px" :size="12">
-                            <Warning />
-                          </el-icon>
-                        </el-tooltip>
-                      </div>
+                      <div style="display: inline-flex; align-items: center">Confirmed deaths</div>
                     </template>
-                  </el-statistic>
+                  </a-statistic>
                   <div class="statistic-footer">
                     <div class="footer-item">
                       <span>Lat update:2 August 2023 at 07:56 pm GMT+8</span>
                     </div>
                   </div>
                 </div>
-              </el-col>
-              <el-col :span="8">
+              </a-col>
+              <a-col :span="8">
                 <div class="statistic-card">
-                  <el-statistic :value="13492099754" title="New transactions today">
+                  <a-statistic :value="13492099754" title="New transactions today">
                     <template #title>
                       <div style="display: inline-flex; align-items: center">
                         Vaccine doses administrated
                       </div>
                     </template>
-                  </el-statistic>
+                  </a-statistic>
                   <div class="statistic-footer">
                     <div class="footer-item">
                       <span>Last update: 30 July 2023</span>
                     </div>
                   </div>
                 </div>
-              </el-col>
-            </el-row>
-          </el-scrollbar>
-        </el-main>
-      </el-container>
+              </a-col>
+            </a-row>
+          </div>
+        </a-layout-content>
+      </a-layout>
     </div>
 
     <div class="section">
-      <el-row>
-        <el-col :span="2"></el-col>
-        <el-col :span="8">
+      <a-row>
+        <a-col :span="2"></a-col>
+        <a-col :span="8">
           <MapTimeline
             :width-height-ratio="0.6"
             :dates="dates"
@@ -194,31 +201,16 @@ import TrendTimeline from './components/TrendTimeline.vue'
             v-model:selected-date="selectedDate"
             v-model:selected-index="selectedIndex"
           />
-        </el-col>
-        <el-col :span="12" style="width: 800px">
-          <el-carousel :interval="4000" type="card" height="200px">
-            <el-carousel-item v-for="item in 6" :key="item">
-              <h3 text="2xl" justify="center">{{ item }}</h3>
-            </el-carousel-item>
-          </el-carousel>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="8" style="width: 10vw">
+        </a-col>
+        <a-col :span="12" style="width: 800px"> </a-col>
+      </a-row>
+      <a-row>
+        <a-col :span="8" style="width: 10vw">
           <div>
-            <el-timeline style="color: azure">
-              <el-timeline-item
-                v-for="(activity, index) in activities"
-                :key="index"
-                :timestamp="activity.timestamp"
-              >
-                {{ activity.content }}
-                <button>leran more</button>
-              </el-timeline-item>
-            </el-timeline>
+            <!-- timeline -->
           </div>
-        </el-col>
-        <el-col :span="16">
+        </a-col>
+        <a-col :span="16">
           <div>
             <TrendTimeline
               :width-height-ratio="0.5"
@@ -227,8 +219,8 @@ import TrendTimeline from './components/TrendTimeline.vue'
               v-model:selected-index="selectedIndex"
             />
           </div>
-        </el-col>
-      </el-row>
+        </a-col>
+      </a-row>
     </div>
     <div class="section">
       <div class="chart-map">
@@ -240,20 +232,9 @@ import TrendTimeline from './components/TrendTimeline.vue'
 </template>
 
 <style lang="scss">
-.el-row {
-  margin-bottom: 20px;
+.transparent_content {
+  color: 'transparent';
 }
-
-.el-row:last-child {
-  margin-bottom: 0;
-}
-
-.el-col {
-  border-radius: 0px;
-  text-align: center;
-  color: transparent;
-}
-
 .grid-content {
   border-radius: 4px;
   min-height: 36px;
@@ -263,19 +244,11 @@ import TrendTimeline from './components/TrendTimeline.vue'
   margin-bottom: 200px;
 }
 
-:global(h2#card-usage ~ .example .example-showcase) {
-  background-color: var(--el-fill-color) !important;
-}
-
-.el-statistic {
-  --el-statistic-content-font-size: 28px;
-}
-
 .statistic-card {
   height: 100%;
   padding: 20px;
   border-radius: 4px;
-  background-color: var(--el-bg-color-overlay);
+  background-color: #ffffff;
 }
 
 .statistic-footer {
@@ -284,7 +257,7 @@ import TrendTimeline from './components/TrendTimeline.vue'
   align-items: center;
   flex-wrap: wrap;
   font-size: 12px;
-  color: var(--el-text-color-regular);
+  color: black;
   margin-top: 16px;
 }
 
@@ -305,32 +278,11 @@ import TrendTimeline from './components/TrendTimeline.vue'
   padding: 0;
 }
 
-.green {
-  color: var(--el-color-success);
-}
-
-.red {
-  color: var(--el-color-error);
-}
-
 .side-card-box {
   width: 480px;
   border-radius: 0px;
 }
-
-.el-carousel__item h3 {
-  color: #475669;
-  opacity: 0.75;
-  line-height: 200px;
-  margin: 0;
-  text-align: center;
-}
-
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+.scroll_content {
+  overflow-x: hidden;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import './assets/main.css'
 
 
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 import 'vue-fullpage.js/dist/style.css'
 import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(ElementPlus, { size: 'small', zIndex: 3000 })
-app.mount('#app')
+app.use(Antd).mount('#app');
